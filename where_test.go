56bd12a0c4d1eca7e24a90f657aca4c91ebbf871
Subhash Chandran <subwiz@gmail.com>
2025-04-05 10:32:03 +0530

    added tests

--- where_test.go
+++ where_test.go
@@ -31,3 +31,34 @@ func TestWhereBuilderOr(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestWhereBuilderOnlyBetween(t *testing.T) {
+	wb := NewWhereBuilder()
+	wb.AddBetween("order_time", time.Now(), time.Now())
+	partialQry, vals := wb.GenAnd()
+	log.Println(partialQry, "|", len(vals))
+	if partialQry != " WHERE order_time BETWEEN ? AND ?" {
+		t.Fail()
+	}
+}
+
+func TestWhereBuilderOnlyOrdrBy(t *testing.T) {
+	wb := NewWhereBuilder()
+	wb.AddOrdrByCols("salary", "age")
+	partialQry, vals := wb.GenAnd()
+	log.Println(partialQry, "|", len(vals))
+	if partialQry != " ORDER BY salary, age" {
+		t.Fail()
+	}
+}
+
+func TestWhereBuilderPage(t *testing.T) {
+	wb := NewWhereBuilder()
+	wb.AddOrdrByCols("salary", "age")
+	wb.SetPage(2, 20)
+	partialQry, vals := wb.GenAnd()
+	log.Println(partialQry, "|", len(vals))
+	if partialQry != " ORDER BY salary, age LIMIT 20 OFFSET 40" {
+		t.Fail()
+	}
+}
