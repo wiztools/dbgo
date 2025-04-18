db0a73389f7639f64529b7fcc33bbb227cc9fbe1
Subhash Chandran <subwiz@gmail.com>
2025-04-18 03:14:01 -0700

    more Scan*() fns added

--- dbgo-scanner.go
+++ dbgo-scanner.go
@@ -1,5 +1,7 @@
 package dbgo
 
+import "time"
+
 // Scanner is an interface that defines the Scan method that is part of sql.Rows and sql.Row.
 // This helps modularize the scanning code to one function that can be reused across different types.
 type Scanner interface {
@@ -12,8 +14,32 @@ func ScanInt(row Scanner) (*int64, error) {
 	return &id, err
 }
 
+func ScanFloat(row Scanner) (*float64, error) {
+	var f float64
+	err := row.Scan(&f)
+	return &f, err
+}
+
 func ScanString(row Scanner) (*string, error) {
 	var str string
 	err := row.Scan(&str)
 	return &str, err
+}
+
+func ScanBool(row Scanner) (*bool, error) {
+	var b bool
+	err := row.Scan(&b)
+	return &b, err
+}
+
+func ScanTime(row Scanner) (*time.Time, error) {
+	var t time.Time
+	err := row.Scan(&t)
+	return &t, err
+}
+
+func ScanBytes(row Scanner) (*[]byte, error) {
+	var b []byte
+	err := row.Scan(&b)
+	return &b, err
 }
